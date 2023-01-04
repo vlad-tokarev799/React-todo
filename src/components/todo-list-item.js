3a342d69e50b290e609a6bac2b35c1f880f172f7
vlad-tokarev799 <vlad.tokarev.200247@gmail.com>
2023-01-05 01:26:31 +0800

    Закончил 1.3.12

--- src/components/todo-list-item.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TodoListItem = (props) => {
-    return (
-        <span className={props.important ? 'important' : null}>{props.label}</span>
-    )
-}
-
-export default TodoListItem
--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import AppHeader from "./components/app-header"
-import SearchPanel from "./components/search-panel"
-import TodoList from "./components/todo-list"
+import App from './components/app'
 
-
-
-const App = () => {
-
-    const todoData = [
-        {
-            label: 'Learn React',
-            important: false,
-            id: 1
-        },
-        {
-            label: 'Make Awesome App',
-            important: true,
-            id: 2
-        },
-        {
-            label: 'Drink Coffee',
-            important: false,
-            id: 3
-        }
-    ]
-
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={ todoData } />
-        </div>
-    )
-}
-
-const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<App />)
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+root.render(<App />);
